4dabbf995ee27a941bcd63e5b965501ef9dc91d8
MartinMeng1 <126767342+MartinMeng1@users.noreply.github.com>
2023-08-24 18:32:43 -0700

    added changes to all files

---
+++ client/src/components/NavTabs.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const styles = {
+  card: {
+    margin: 20,
+    background: '#6E6E6E',
+    padding: '10px'
+  },
+  heading: {
+    lineHeight: 3,
+    fontSize: '2rem',
+    padding: '0 20px',
+    display: 'inline-block',
+  },
+  font: {
+    color: '#BAFF39',
+    margin: 'theme.spacing(5)',
+  }
+};
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul style={styles.card}>
+      <li style={styles.heading}>
+        <a
+          href="#main"
+          onClick={() => handlePageChange('MainPage')}
+
+          style={styles.font}
+        > Home
+        </a>
+      </li>
+      <li style={styles.heading}>
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          style={styles.font}
+        > Resume
+        </a>
+      </li>
+      <li style={styles.heading}>
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+
+          style={styles.font}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li style={styles.heading}>
+        <a
+          href="#contact"
+
+          onClick={() => handlePageChange('Contact')}
+          style={styles.font}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Body = require('./Body');
+const Food = require('./Food')
+
+module.exports = {User,Body,Food}
